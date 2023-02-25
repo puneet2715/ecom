9d633eba13f75da6a67a556f31653a24e0136a8e
Puneet <puneet2715@gmail.com>
2023-02-25 08:24:47 +0530

    test setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.jest.json',
+      },
+    ],
+  },
+  testMatch: ['**/**/?(*.)+(spec|test).[tj]s?(x)'],
+  setupFilesAfterEnv: ['./src/jest.setup.ts'],
+};
